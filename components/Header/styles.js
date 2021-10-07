c86cc1833440215688e40ae73372a7e246207d4f
Davi Lopes Teixeira <davilteixeira@edu.unifor.br>
2021-10-07 17:34:49 -0300

    first commit

---
+++ components/Header/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    padding: 5px;
+    width: 100%;
+    height: 100px;
+    border-bottom-color: black;
+    border-bottom-width: 1px;
+`
---
+++ pages/Fipe/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Title, Container } from './styles'
+function Fipe() {
+    return(
+        <Container>
+            <Title>teste</Title>
+        </Container>
+    )
+}
+
+export default Fipe;
